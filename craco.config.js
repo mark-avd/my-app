b18b4b786e43591580049eddf4ef64246e4f1c3b
Mark Avdeev <printmoonges@yandex.com>
2021-12-01 00:14:59 +0700

    init commit

---
+++ craco.config.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const CracoLinariaPlugin = require('craco-linaria')
+
+module.exports = {
+    plugins: [
+        {
+            plugin: CracoLinariaPlugin,
+            options: {
+                // Linaria options
+            },
+        },
+    ],
+}
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import HomePage from './pages/HomePage'
+
+function App(): React.ReactElement {
+    return <><HomePage /></>
+}
+
+export default App
